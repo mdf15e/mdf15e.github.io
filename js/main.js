c6a0b6f1a1ee311dfafb7083fcccd990118777ff
User <User@email.com>
2025-05-08 23:21:28 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,184 @@
+import { extractHtmlSnippet, insertContentHeading, loadSnsLinks, setFavicon, switchLang, wrapContentTitle, wrapDesignedText} from './utils.js';
+
+let siteSettings = {};  // サイトの設定情報を格納する変数
+let t = {};  // 翻訳データ
+let lang = 'ja';  // 言語設定
+
+// 設定ファイルと翻訳ファイルの読み込み
+Promise.all([
+  fetch('/data/site-settings.json').then(response => response.json()),
+  fetch('/i18n/for_js.json').then(response => response.json())
+])
+  .then(([config, allTexts]) => {
+    siteSettings = config;
+    const pathLang = location.pathname.split('/')[1];
+    lang = siteSettings.langs.includes(pathLang) ? pathLang : 'ja';
+    t = allTexts[lang] || allTexts['ja'];
+
+    setFavicon(siteSettings.iconImage);
+    if (!document.title || document.title === '') document.title = siteSettings.siteTitle;
+
+    wrapContentTitle();
+    observeContentTitles();
+    wrapDesignedText();
+    observeDesignedText();
+
+    readPage();  // ページの読み込み処理を呼び出す
+  })
+  .catch(err => {
+    console.error('Error loading settings or translation:', err);
+  });
+
+// ページの読み込み
+function readPage() {
+  applyTheme();
+  loadHeader();
+  loadFooter();
+  loadSnsLinks(siteSettings.sns);
+  loadArticles();
+  insertContentHeading(siteSettings.contentTypes);
+}
+
+// テーマの設定
+function applyTheme() {
+  const root = document.documentElement;
+
+  root.style.setProperty('--theme-dark', siteSettings.colors.themeDarkColor);
+  root.style.setProperty('--theme-light', siteSettings.colors.themeLightColor);
+  root.style.setProperty('--theme-transparent-dark', siteSettings.colors.themeTransparentDarkColor);
+  root.style.setProperty('--theme-transparent-light', siteSettings.colors.themeTransparentLightColor);
+  root.style.setProperty('--theme-text', siteSettings.colors.themeTextColor);
+  root.style.setProperty('--accent-color', siteSettings.colors.accentColor);
+  root.style.setProperty('--title-font', siteSettings.fonts.titleFont);
+  root.style.setProperty('--body-font', siteSettings.fonts.bodyFont);
+  root.style.setProperty('--icon-image', `url(${siteSettings.iconImage})`);
+}
+
+// headerの読み込み
+function loadHeader() {
+  fetch('/includes/header.html')
+    .then(response => response.text())
+    .then(html => {
+      // const siteName = wrapInitialLetters(siteSettings.siteName);
+      html = html
+        .replace(/{{siteName}}/g, siteSettings.siteName)
+        .replace(/{{lang}}/g, lang)
+      document.getElementById('header').innerHTML = html;
+
+      setupLanguageSwitcher();
+      loadSnsLinks(siteSettings.sns);
+    })
+    .catch(err => console.error('Error loading header:', err));
+}
+
+// footerの読み込み
+function loadFooter() {
+  const footer = document.getElementById('footer');
+  if (!footer) return;
+
+  fetch('/includes/footer.html')
+    .then(response => response.text())
+    .then(html => {
+      footer.innerHTML = html;
+      loadSnsLinks(siteSettings.sns);
+    })
+    .catch(err => console.error('Error loading footer:', err));
+}
+
+// 記事一覧の作成
+function loadArticles() {
+  const articleList = document.getElementById('article-list');
+  if (!articleList) return;
+
+  fetch('/data/articles.json')
+    .then(response => response.json())
+    .then(allArticles => {
+      const articles = allArticles
+        .filter(article => article.lang.includes(lang))
+        .sort((a, b) => new Date(b.created) - new Date(a.created));
+
+      fetch('/includes/article-summary.html')
+        .then(response => response.text())
+        .then(template => {
+          articles.forEach(file => {
+            fetch("articles/" + file.filename)
+              .then(response => response.text())
+              .then(html => {
+                const tempDiv = document.createElement('div');
+                tempDiv.innerHTML = html;
+                const title = tempDiv.querySelector('.content-title')?.textContent.trim() || t.noTitle;
+                const mainText = tempDiv.querySelector('.content-text') || (() => {
+                  const fallback = document.createElement('div');
+                  fallback.innerHTML = t.noIntro;
+                  return fallback;
+                })();
+                length = siteSettings.introLength[lang] ?? siteSettings.introLength.default;
+
+                const intro = extractHtmlSnippet(
+                  mainText,
+                  length,
+                  ` <a href="articles/${file.filename}" class="read-more-link">......${t.readMore}</a>`
+                );
+
+                const summary = template
+                  .replace(/{{title}}/g, title)
+                  .replace(/{{intro}}/g, intro)
+                  .replace(/{{link}}/g, "articles/" + file.filename)
+
+                const summaryDiv = document.createElement('div');
+                summaryDiv.innerHTML = summary;
+                articleList.appendChild(summaryDiv);
+              })
+              .catch(err => console.error('Error loading article:', err));
+          });
+        })
+        .catch(err => console.error('Error loading article template:', err));
+    })
+    .catch(err => console.error('Error loading articles data:', err));
+}
+
+// 言語切り替えボタンの設定
+function setupLanguageSwitcher() {
+  const buttons = document.querySelectorAll('#language-switcher button');
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const targetLang = btn.dataset.lang.toLowerCase();
+      switchLang(targetLang, siteSettings.langs);
+    });
+  });
+}
+
+function observeContentTitles() {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      mutation.addedNodes.forEach(node => {
+        if (node.nodeType !== 1) {return;} else {
+          if (node.matches('.content-title') || node.querySelector('.content-title')) {
+            wrapContentTitle();
+          }
+        };
+      });
+    });
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+}
+
+function observeDesignedText() {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      mutation.addedNodes.forEach(node => {
+        if (node.nodeType !== 1) return;
+
+        if (node.matches('.designed') || node.querySelector('.designed')) {
+          wrapDesignedText();
+        }
+      });
+    });
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+}
---
+++ js/utils.js
@@ -0,0 +1,224 @@
+export function extractHtmlSnippet(domNode, limit = 100, suffix = '') {
+  let count = 0;
+  let reachedLimit = false;
+
+  function cloneWithLimit(node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+      if (reachedLimit) return null;
+
+      const text = node.textContent;
+      const remaining = limit - count;
+
+      if (text.length <= remaining) {
+        count += text.length;
+        return document.createTextNode(text);
+      } else {
+        count += remaining;
+        reachedLimit = true;
+        return document.createTextNode(text.slice(0, remaining));
+      }
+    }
+
+    if (node.nodeType === Node.ELEMENT_NODE) {
+      if (node.tagName.toLowerCase() === 'p' || node.tagName.toLowerCase() === 'div' && node.classList.contains('content-text')) {
+        const fragment = document.createDocumentFragment();
+        for (const child of node.childNodes) {
+          const limitedChild = cloneWithLimit(child);
+          if (limitedChild) fragment.appendChild(limitedChild);
+          if (reachedLimit) break;
+        }
+        return fragment;
+      }
+
+      const clone = node.cloneNode(false);
+      for (const child of node.childNodes) {
+        const limitedChild = cloneWithLimit(child);
+        if (limitedChild) clone.appendChild(limitedChild);
+        if (reachedLimit) break;
+      }
+      return clone;
+    }
+
+    return null;
+  }
+
+  const snippetNode = cloneWithLimit(domNode);
+  if (!snippetNode) return '';
+
+  const wrapper = document.createElement('div'); // 外側に1つだけ <p>
+  wrapper.appendChild(snippetNode);
+  if (reachedLimit && suffix) {
+    const suffixSpan = document.createElement('span');
+    suffixSpan.innerHTML = suffix;
+    wrapper.appendChild(suffixSpan);
+  }
+
+  return wrapper.outerHTML;
+}
+
+export function insertContentHeading(typeToLabel) {
+  document.querySelectorAll('div.content[data-content-type]').forEach(div => {
+    const previous = div.previousElementSibling;
+    if (previous?.classList.contains('content-heading')) return;
+
+    const type = div.dataset.contentType?.toLowerCase();
+    const label = typeToLabel[type];
+
+    if (!label) return;
+
+    const heading = document.createElement('h2');
+    heading.className = 'designed content-heading';
+    heading.textContent = label;
+
+    div.parentNode.insertBefore(heading, div);
+  });
+}
+
+export function loadSnsLinks(snsdata){
+  const snsLinks = document.querySelectorAll('.sns-link');
+  if (snsLinks.length === 0) return;
+
+  snsLinks.forEach(link => {
+    const platform = link.dataset.platform.toLowerCase();
+    
+    if(snsdata[platform] && snsdata[platform].url && snsdata[platform].url !== ''){
+      const url = snsdata[platform].url;
+
+      const a = document.createElement('a');
+      a.href = url;
+      a.target = '_blank';
+      a.rel = 'noopener noreferrer';
+
+      const icon = snsdata[platform].icon;
+      if(icon){
+        const img = document.createElement('img');
+        img.src = icon;
+        img.alt = platform;
+        img.style.width = '1.5em';
+        img.style.height = '1.5em';
+        img.style.margin = '0 0.2em';
+        a.appendChild(img);
+      } else {
+        a.textContent = platform;
+      };
+
+      link.replaceWith(a);
+    } else {
+      console.warn(`SNS link for ${platform} not found in site settings.`);
+    }
+  })
+}
+
+export function setFavicon(url, type) {
+  let favicon = document.querySelector('link[rel="icon"]');
+  if (favicon) return;
+
+  favicon = document.createElement('link');
+  favicon.rel = 'icon';
+  favicon.href = url;
+  favicon.type = type ? type : 'image/' + url.split('.').pop();
+  document.head.appendChild(favicon);
+}
+
+export function switchLang(targetLang, supportedLangs) {
+    const currentPath = location.pathname;
+    const currentLang = currentPath.split('/')[1];
+  
+    if (!supportedLangs.includes(targetLang)) {
+      console.warn('Unsupported language:', targetLang);
+      return;
+    }
+
+    let newPath;
+    if (supportedLangs.includes(currentLang)) {
+      newPath = currentPath.replace(`/${currentLang}/`, `/${targetLang}/`);
+    } else {
+      newPath = `/${targetLang}${currentPath}`;
+    }
+  
+    location.pathname = newPath;
+}
+
+export function wrapContentTitle() {
+  const titles = document.querySelectorAll('.content .content-title');
+  titles.forEach(title => {
+    if (title.querySelector('.first-letter')) return;
+
+    function findFirstTextNode(node) {
+      for (let child of node.childNodes) {
+        if (child.nodeType === Node.TEXT_NODE && child.textContent.trim().length > 0) {
+          return child;
+        } else if (child.nodeType === Node.ELEMENT_NODE) {
+          const result = findFirstTextNode(child);
+          if (result) return result;
+        }
+      }
+      return null;
+    }
+
+    const textNode = findFirstTextNode(title);
+    if (!textNode) return;
+
+    const text = textNode.textContent.trim();
+    if (!text || text.length === 0) return;
+
+    const firstLetter = text.charAt(0);
+    const rest = text.slice(1);
+
+    const span = document.createElement('span');
+    span.className = 'first-letter';
+    span.textContent = firstLetter;
+
+    const restText = document.createTextNode(rest);
+
+    const parent = textNode.parentNode;
+    parent.insertBefore(span, textNode);
+    parent.insertBefore(restText, textNode);
+    parent.removeChild(textNode);
+  });
+}
+
+export function wrapDesignedText() {
+  const targets = document.querySelectorAll('.designed');
+
+  targets.forEach(el => {
+    if (el.querySelector('.initial')) return;
+
+    const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, {
+      acceptNode: node => {
+        if (node.textContent.trim()) return NodeFilter.FILTER_ACCEPT;
+        return NodeFilter.FILTER_REJECT;
+      }
+    });
+
+    const textNodes = [];
+    while (walker.nextNode()) {
+      textNodes.push(walker.currentNode);
+    }
+
+    textNodes.forEach(textNode => {
+      const parts = textNode.textContent.split(/([ \-･]+)/);
+
+      const fragment = document.createDocumentFragment();
+
+      parts.forEach(part => {
+        if (part.match(/([ \-･]+)/)) {
+          fragment.appendChild(document.createTextNode(part));
+        } else if (part.length > 0) {
+          const span = document.createElement('span');
+          span.className = 'initial';
+          span.textContent = part.charAt(0);
+          fragment.appendChild(span);
+          if (part.length > 1) {
+            fragment.appendChild(document.createTextNode(part.slice(1)));
+          }
+        }
+      });
+
+      textNode.parentNode.replaceChild(fragment, textNode);
+    });
+
+    el.classList.remove('designed');
+    el.classList.add('design-done');
+  });
+}
